1079c845913d90d1c4441ae2571fb67e6be736ed
Brett Commandeur <bcmdr.co@gmail.com>
2018-08-25 07:39:57 -0600

    sucessfully implement background audio

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <background-audio></background-audio>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import BackgroundAudio from '@/components/BackgroundAudio'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BackgroundAudio
   }
 }
 </script>
