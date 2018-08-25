db99cc0e4af9f845303e563a9978c222256fc361
Brett Commandeur <bcmdr.co@gmail.com>
2018-08-25 11:15:24 -0600

    playtest 1

--- src/components/GameOutput.vue
+++ src/components/GameOutput.vue
@@ -4,7 +4,45 @@
 
 <script>
 export default {
-
+  data () {
+    story: {
+      chapter: {
+        one: [
+          "You have died.",
+          "You see a man.",
+          "He is walking away.",
+          "He is carrying a light.",
+          "The light is dimming.",
+          "There are four walls.",
+          "The windows are boarded.",
+          "You cannot leave.",
+          "You are following the man.",
+          "You enter a hallway.",
+          "You find a lighter.",
+          "The man is gone.",
+          "You are alone.",
+          "It is cold.",
+          "Get out. Before he does."
+        ]
+      }
+      prompts: {
+        chapter: {
+          one: [
+            "You don't know.",
+            "You are dead.",
+          ]
+        }
+      }
+      options: {
+        chapter: {
+          one: [
+            "Stay",
+            "Chase",
+          ]
+        }
+      }
+    }
+  }
 }
 </script>
 
